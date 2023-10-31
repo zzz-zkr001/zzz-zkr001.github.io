05de881c5a40b50ac3f7a7f724400d25a3f0b8b2
zhaokeran <1662252279@qq.com>
2023-10-31 10:38:20 +0800

    Site updated: 2023-10-31 10:38:20

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","52168a96d80dce17b539d6bb4aa69491"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
